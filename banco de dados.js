dac6f652758cd7ce6b3f9953888f50f2d8ebd274
Alves <elnataalves12@gmail.com>
2026-09-08 11:51:04 -0300

    Princípios

---
+++ banco de dados.js
@@ -0,0 +1,24 @@
+{
+  "welcome": {},
+  "welcomeMsg": {},
+  "antilink": {},
+  "antilinkBan": {},
+  "mutados": {},
+  "rankAtivo": {},
+  "nivelAtivo": {},
+  "afk": {},
+  "donos": [],
+  "regras": {},
+  "interactionGifs": {
+    "tapa": "",
+    "matar": "",
+    "beijar": "",
+    "abracar": "",
+    "socar": ""
+  },
+  "warning": {},
+  "antiSpam": {},
+  "temporizador": {},
+  "nivelXp": {},
+  "levelConfig": {}
+}
